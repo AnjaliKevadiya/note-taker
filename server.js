d5bafb4cef0ccf183e5fd6510614469d3fad7384
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-11-28 22:53:22 -0500

    created server

---
+++ server.js
@@ -0,0 +1,22 @@
+//include express dependencies
+var express = require("express");
+
+//tell node to create server
+var app = express();
+
+//sets an initial port
+var PORT = process.env.PORT || 8080;
+
+//sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+//routers
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
