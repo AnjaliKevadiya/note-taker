0928a39580677c73add852e2694b250c777e668d
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-11-30 18:40:55 -0500

    added start module inside scripts in package.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,8 +9,19 @@ module.exports = function(app) {
 
     app.post("/api/notes", function(req, res) {
 
-        console.log("data" + req.body);
+        let id;
+        if(notesData.length > 0) {
+            id = notesData[notesData.length - 1].id + 1;
+        } else {
+            id = 1
+        }
+        req.body.id = id;
         notesData.push(req.body);
+        console.log("data" + JSON.stringify(notesData));
         res.json(true);
     });
+
+    // app.delete("/api/notes/:id", function(req, res) {
+    //     console.log("delete data" + req.body);
+    // });
 }
