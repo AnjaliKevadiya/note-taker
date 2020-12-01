afb381c47b297a802e19066a4771e357b070a4b2
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-11-30 20:30:26 -0500

    created store class file

---
+++ db/store.js
@@ -0,0 +1,13 @@
+const util = require("util");
+const fs = require("fs");
+
+const uuidv1 = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+
+}
+
+module.exports = new Store();
